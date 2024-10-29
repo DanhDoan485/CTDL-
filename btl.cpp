f5027b935adb4b5c064f4d2d7f0cee03577b468f
DanhDoan485 <danhgit50@gmail.com>
2024-10-29 23:23:41 +0700

    check

--- btl.cpp
+++ btl.cpp
@@ -11,12 +11,12 @@ class Node{
     Node(int heso, int bac): he_so(heso), b_ac(bac), next(nullptr) {}
 };
 
-class Polinomial{
+class Da_thuc{
     private:
     std::shared_ptr<Node> head;
     public:
-    Polinomial(): head(nullptr){}
-
+    Da_thuc(): head(nullptr){}
+    //Nhập vào hệ số, bậc
     void input(){
         int heso, bac;  
         char luachon;
@@ -27,4 +27,37 @@ class Polinomial{
         }
         while(luachon == 'y' || luachon == 'Y');
     }
+    //Thiết lập biểu thức
+    void insert(int heso, int bac){
+
+        if(heso == 0) return;
+        auto newNode = std::make_shared<Node>(heso, bac);
+        if(!head || head ->b_ac < bac){
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+
+        auto current = head;
+        while(current -> next && current -> next -> b_ac > bac){
+            current = current ->next;
+        }
+
+        if(current -> b_ac == bac){
+            current -> he_so += heso;
+            newNode -> next = current-> next;
+            current -> next = newNode;
+        }
+    }
+    //In ra màn hình
+    void display(int heso, int bac){
+        auto current = head;
+        while(current){
+            std::cout << current -> he_so << "x^" << current -> b_ac;
+            current = current -> next;
+            if(current) std::cout << "+"; 
+        }
+        std::cout << std::endl;
+    }
+    //Tính biểu thức
 };
